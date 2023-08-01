c0fd6e52bddd112f9b6a4ea69c2d6f41af3d7415
Victor Vaz <victorvaz001@gmail.com>
2023-08-01 17:42:34 -0300

    Recuperando dados do banco de dados com o metodo 'find'

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -9,20 +9,16 @@ import dominio.Pessoa;
 public class Programa {
 
 	public static void main(String[] args) {
-		
-		Pessoa p1 = new Pessoa(null, "Carlosda da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Carlosda da Silva", "carlos@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Carlosda da Silva", "carlos@gmail.com");
-		
+
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();		
-		System.out.println("Pronto!");
-		
-	}
 
+		Pessoa p = em.find(Pessoa.class, 2);
+
+		System.out.println(p);
+
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
+	}
 }
