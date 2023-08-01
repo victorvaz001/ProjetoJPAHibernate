5bab35ec6cd6449a413e5da5d68ef1789f8113ee
Victor Vaz <victorvaz001@gmail.com>
2023-08-01 17:37:55 -0300

    Criação e persistencia de dados com o EntityManagerFactory e EntityManager

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -1,19 +1,28 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(1, "Carlosda da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Carlosda da Silva", "carlos@gmail.com");
-		Pessoa p3 = new Pessoa(3, "Carlosda da Silva", "carlos@gmail.com");
+		Pessoa p1 = new Pessoa(null, "Carlosda da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Carlosda da Silva", "carlos@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Carlosda da Silva", "carlos@gmail.com");
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
-
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();		
+		System.out.println("Pronto!");
+		
 	}
 
 }
